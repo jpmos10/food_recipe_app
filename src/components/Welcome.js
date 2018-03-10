df091d9002702cffc00114fd736a7b48397c8186
JessicaSung <18690072+JessicaSung@users.noreply.github.com>
2018-03-09 20:30:01 -0600

    added additional setup files

---
+++ src/components/Welcome.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Welcome extends Component {
+    render() {
+        return (
+            <h3>Tasty Tech</h3>
+        );
+    }
+}
